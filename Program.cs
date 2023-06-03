4ed576c9837e731a37c0f607d7a57814005f4fcf
Jakub146480 <128227074+Jakub146480@users.noreply.github.com>
2023-06-03 15:15:58 +0200

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+public class zadanie1
+{
+    public static void Main(string[] args)
+    {
+        int a, b, c;
+
+        a = (int)inputValue();
+        b = (int)inputValue();
+        c = (int)inputValue();
+        int sredniaArytmetyczna;
+
+        sredniaArytmetyczna = (int)((double)(a + b + c) / 2);
+        Console.WriteLine(sredniaArytmetyczna);
+    }
+
+    // .NET can only read single characters or entire lines from the
+    // console. The following function safely reads a double value.
+    private static double inputValue()
+    {
+        double result;
+        while (!double.TryParse(Console.ReadLine(), out result)) ;
+        return result;
+    }
+}
+
